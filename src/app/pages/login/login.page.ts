36c88586dbe88f504188e2fcca6c885e9198b0d5
Samuel <samuelalves681@gmail>
2021-11-16 23:02:25 -0300

    service login e armazenar token no storage

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { IonRouterOutlet, ModalController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  AlertController,
+  IonRouterOutlet,
+  LoadingController,
+  ModalController,
+} from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
 import { RegisterPage } from '../register/register.page';
 
 @Component({
@@ -8,12 +16,23 @@ import { RegisterPage } from '../register/register.page';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  credentials: FormGroup;
   constructor(
     private modalCtrl: ModalController,
-    private routerOutlet: IonRouterOutlet
+    private routerOutlet: IonRouterOutlet,
+    private fb: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private apiService: ApiService,
+    private router: Router,
+    private alert: AlertController
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.credentials = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 
   async openRegister() {
     const modal = await this.modalCtrl.create({
@@ -22,5 +41,41 @@ export class LoginPage implements OnInit {
     });
 
     await modal.present();
+  }
+
+  async login() {
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    this.apiService.login(this.credentials.value).subscribe(
+      async (res) => {
+        await loading.dismiss();
+        await this.modalCtrl.dismiss();
+        this.router.navigateByUrl('/inside', { replaceUrl: true });
+      },
+      async (err) => {
+        await loading.dismiss();
+
+        const alert = await this.alert.create({
+          header: 'Login Failed',
+          message: err.error.msg,
+          buttons: ['ok'],
+        });
+
+        await alert.present();
+      }
+    );
+  }
+
+  close() {
+    this.modalCtrl.dismiss();
+  }
+
+  get username() {
+    return this.credentials.get('username');
+  }
+
+  get password() {
+    return this.credentials.get('password');
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,14 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { map, switchMap } from 'rxjs/operators';
+import jwtDecode from 'jwt-decode';
+
+import { Storage } from '@capacitor/storage';
+import { from } from 'rxjs';
+
+const TOKEN_KEY = 'access-token';
+const REFRESH_TOKEN = 'refresh-token';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  token = '';
+  userId = null;
   constructor(private http: HttpClient) {}
 
   signUp(credentials: { username; password }) {
     return this.http.post(`${environment.apiUrl}/users`, credentials);
+  }
+
+  login(credentials: { username; password }) {
+    return this.http.post(`${environment.apiUrl}/auth`, credentials).pipe(
+      switchMap((data: any) => {
+        console.log(data);
+        this.token = data.accessToken;
+        const decoded: any = jwtDecode(this.token);
+        console.log('decoded: ', decoded);
+        this.userId = decoded.id;
+
+        const storedTokens = Promise.all([
+          Storage.set({ key: TOKEN_KEY, value: this.token }),
+          Storage.set({ key: REFRESH_TOKEN, value: data.refreshToken }),
+        ]);
+        return from(storedTokens);
+      })
+    );
   }
 }
